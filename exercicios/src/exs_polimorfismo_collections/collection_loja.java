e662ed382304ac1304317f0f5334f60a406b4582
unknown <danielcds_@live.com>
2020-08-31 22:03:10 -0300

    subindo alteracoes

--- exercicios/src/exs_polimorfismo_collections/collection_loja.java
+++ exercicios/src/exs_polimorfismo_collections/collection_loja.java
@@ -1,7 +1,7 @@
 /*
-Crie uma um programa para trabalhar com estoque de uma loja, o programa deverï¿½
+Crie uma um programa para trabalhar com estoque de uma loja, o programa deverá
 trabalhar com Collection do tipo List do Java para manipular os dados desse estoque, o
-programa deverï¿½ atender as seguintes funcionalidades:
+programa deverá atender as seguintes funcionalidades:
 Armazenar dados da List
 Remover dados da list;
 Atualizar dados da list.
