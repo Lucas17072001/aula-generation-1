18fa500cd4728f5b48dae2625ee70f9f83e57d2f
unknown <danielcds_@live.com>
2020-09-08 17:17:21 -0300

    subindo ex Farmacia API

---
+++ loja/src/main/java/com/farmacia/loja/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.farmacia.loja.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farmacia.loja.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
+}
+
