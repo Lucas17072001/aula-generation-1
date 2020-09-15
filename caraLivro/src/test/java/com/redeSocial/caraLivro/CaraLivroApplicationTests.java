c13bdc92e52e6dc18273dfaea603511153a5f35d
unknown <danielcds_@live.com>
2020-09-15 18:25:08 -0300

    up blog e redes ocial

---
+++ caraLivro/src/test/java/com/redeSocial/caraLivro/CaraLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeSocial.caraLivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaraLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
